fc31e48b5a44f28d6378a5a58dc38a90e306f7c8
augustinhocquet <augustinhocquet@172.17.34.154>
2023-04-01 13:39:45 +0200

    je commit

--- src/main/java/fr/insa/holler/projet_56/Projet_56.java
+++ src/main/java/fr/insa/holler/projet_56/Projet_56.java
@@ -12,6 +12,28 @@ import java.io.FileWriter;
  * @author yohann
  */
 public class Projet_56 {
+    
+    public static void ecriture(String nom_donnee, double pointE) 
+    {
+        String pointEs ;
+        pointEs=Double.toString(pointE);
+         
+        
+        try
+            {
+          BufferedWriter out=new BufferedWriter(new FileWriter("mur.txt",true));
+
+          out.write(nom_donnee + " : ");
+          out.write(pointEs);
+          out.newLine();
+
+          out.close();
+          }
+          catch (IOException err)
+          {
+              System.out.println("Erreur :\n"+err);
+          }
+    }
 
     public static void main(String[] args) {
      
@@ -19,25 +41,14 @@ public class Projet_56 {
         int i;
         String [] tabligne = new String [30];
         String revet;
-        double prixM2;
+        String prixM2;
         double surface;
         
         
         i=1;
-        prixM2=0;
-    
-   /*Point p1,p2;
-   Mur M1;
-   double a,b;
-   p1=new Point(1,10.75,25.1);
-   p2=new Point(2,10,25);
-   p1.afficher();
-   p2.afficher();
-   M1=new Mur(1,p1,p2);
-   a= +M1.longueur();
-   System.out.println(a);
-   b= +M1.surface();
-   System.out.println(b);*/
+        prixM2="0";
+        surface=3;
+   
    
    /*System.out.println("Combien de batiment ? ");
    nbBat=Lire.i();*/
@@ -49,35 +60,44 @@ public class Projet_56 {
    B1.afficher();
    
    
+   Point p1,p2;
+   Mur M1;
+   double a,b;
    
    
+   p1=new Point(1,10.75,25.1);
+   p2=new Point(2,10,25);
+   p1.afficher();
+   p2.afficher();
+   
+   M1=new Mur(1,p1,p2,0,0);
+   a= +M1.longueur();
+   System.out.println(a);
+   b= +M1.surface();
+   System.out.println(b);
+   
+   
+   System.out.println("");
+   ecriture("x1" ,p1.px);
+   ecriture("x2" ,p2.py);
+   System.out.println("");
+   ecriture("longueur" ,a);
+   ecriture("surface" , b);
    
    /*File fichmur = new File("mur.txt");
    
    System.out.println(fichmur.getAbsolutePath());
-   
-   try
-   {
- BufferedWriter out=new BufferedWriter(new FileWriter("mur.txt",true));
- 
- 
- out.write();
- out.newLine();
- 
- out.close();
- }
- catch (IOException err)
- {
-     System.out.println("Erreur :\n"+err);
- }
-   
    */
    
+  
+   /*
    
   System.out.println(" quel revetement  ");
   revet=Lire.S();
   System.out.println("surface du sol");
   surface=Lire.d();
+   
+   
    
    try 
    {
@@ -90,12 +110,17 @@ public class Projet_56 {
  
    while((ligne=in.readLine())!=null)
     {
-      tabligne = ligne.split("\n");
+      tabligne = ligne.split(";");
       if ((tabligne[1].compareTo(revet) == 0) && (tabligne[3].compareTo("1")==0)){
-          prixM2=Integer.parseInt(tabligne[3]);
-          
+          prixM2=tabligne[5];
+           System.out.println(tabligne[5]);
+          System.out.println(prixM2 );
+           
+
         
       }
+       
+
      }
    
 in.close();
@@ -109,15 +134,11 @@ in.close();
  System.out.println(" Erreur :\n "+err);}
   
    
- 
-  
+   double pm2 = Double.parseDouble(prixM2);
 
-  
- 
-     System.out.println(surface*prixM2);
+      System.out.println(pm2*surface);
 
-     
-    
+    */
     
     
     }     
