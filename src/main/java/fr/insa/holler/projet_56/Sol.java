18af78e70ad3af707e633d398faf982d5e878ba7
augustinhocquet <augustinhocquet@172.17.34.154>
2023-04-06 23:28:15 +0200

    je commit

--- src/main/java/fr/insa/holler/projet_56/Sol.java
+++ src/main/java/fr/insa/holler/projet_56/Sol.java
@@ -10,16 +10,28 @@ import java.util.ArrayList;
  */
 public class Sol {
     int idSol ;
-    ArrayList <Point> listePoints = new ArrayList<Point>() ;
-    ArrayList <Revetement> listeRevetements = new ArrayList<Revetement>() ;
+    String revetement;
+    Point p1, p2;
+   
     
-    Sol(int id) {
+    Sol(int id, String revet, Point p1, Point p2) {
         this.idSol = id ;
+        this.revetement=revet;
+        this.p1=p1;
+        this.p2=p2;
+        
     }
     void afficher() {
         System.out.println("Piece : idSol : "+this.idSol) ;
+        p1.afficher();p2.afficher();
     }
     
+    
+    double surface()
+    {
+        
+        return Math.sqrt(-(this.p1.py)*(this.p1.py)+(this.p2.py)*(this.p2.py))*Math.sqrt(-(this.p1.px)*(this.p1.px)+(this.p2.px)*(this.p2.px));
+    }
     
 /*  Méthodes :
     afficher()
