f64b8002b6a77ea1ac0e01a326f853e147e38acf
augustinhocquet <augustinhocquet@172.17.34.154>
2023-04-07 19:02:54 +0200

    je commit

--- src/main/java/fr/insa/holler/projet_56/Projet_56.java
+++ src/main/java/fr/insa/holler/projet_56/Projet_56.java
@@ -11,6 +11,8 @@ import java.io.FileWriter;
 
 public class Projet_56 {
     
+    // procedure pour la mise en page du dossier du devis "mu" //
+    
     public static void ecriture(String nom_donnee, double valeur) 
     {
         String pointEs ;
@@ -91,7 +93,10 @@ public class Projet_56 {
           }
     }
 
-    public static double prix(double surface, String revet ) 
+    
+      // calcul un prix en fonction du m2 et du revetement //
+      
+      public static double prix(double surface, String revet ) 
     {
         
         
@@ -140,6 +145,9 @@ public class Projet_56 {
          return prix;    
     }
 
+   
+    // ecriture dans le fichier memoire des objets //
+    
     public static void memoire_mur(Mur M, int idP_plus, int id_bat)
     {
         
@@ -158,7 +166,8 @@ public class Projet_56 {
           
           S_id_memoire=Double.toString(M.idMur);
           out.write("M;" + id_bat + ";" + idP_plus + ";" + M.idMur + ";" + "    " + ";" + p1.px + ";" 
-                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(M.surface(), M.revet) + ";");
+                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(M.surface(), M.revet)
+                  + ";" + M.revet + ";" + M.surface());
           
           out.newLine();
 
@@ -189,7 +198,8 @@ public class Projet_56 {
           
          
           out.write("S;" + id_bat + ";" + idP_plus + ";" + S.idSol + ";" + "    " + ";" + p1.px + ";" 
-                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(S.surface(), S.revetement) + ";");
+                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(S.surface(), S.revetement)
+                  + ";"  + S.revetement + ";" + S.surface());
           
           out.newLine();
 
@@ -220,7 +230,8 @@ public class Projet_56 {
           
          
           out.write("Pl;" + id_bat + ";" + idP_plus + ";" + Pl.idPlanfond + ";" + "    " + ";" + p1.px + ";" 
-                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(Pl.surface(), Pl.revetement) + ";");
+                  + p1.py + ";" + p2.px + ";" + p2.py + ";" +  "   " + ";" + prix(Pl.surface(), Pl.revetement) 
+                  + ";" + Pl.revetement + ";" + Pl.surface() + ";" );
           
           out.newLine();
 
@@ -233,6 +244,11 @@ public class Projet_56 {
         
     }
     
+    //-----------//
+      
+      
+    // inutil //
+       
     public static void prix_mur_ecriture(Mur M) 
     {
     
@@ -266,6 +282,10 @@ public class Projet_56 {
          ecriture_espace(3);ecriture_espace(3);ecriture_espace(3);
     
 }
+    
+   
+    // verification de l'existence de la piece dans le fichier memoire //
+    
     
     public static boolean verification_non_existence(int idp, int idB)
     {
@@ -314,7 +334,10 @@ public class Projet_56 {
         return verif;
     }
     
-   public static void creation_piece(int idP_plus, int idB)
+  
+    // creation d'une piece avec deux point et les revetement ( creation mur sol et planfond )  //
+    
+    public static void creation_piece(int idP_plus, int idB)
    {
     
    Point p1,p2,p3,p4;
@@ -522,12 +545,19 @@ public class Projet_56 {
         
    }
     
-   public static void creation_bat(int idB_plus)
+  
+    
+// creation batiment inutile pour l'instant //
+    
+    public static void creation_bat(int idB_plus)
    {
         Batiment B1;
         B1=new Batiment(idB_plus);
         B1.afficher();
    }
+    
+    
+    // retourne le prix total d'une piece //
    
    public static double total_piece(int idp, int idB)
    {
@@ -579,7 +609,101 @@ public class Projet_56 {
       
    }
    
-    public static void main(String[] args) {
+   
+   
+   public static double prix_revet(String revet)
+   {
+       double prix_R , double_prix; prix_R=0;
+       String [] tabligne = new String [30];
+       
+       
+      try 
+             {
+             BufferedReader in=new BufferedReader(new FileReader("memoire.txt"));
+             
+             String ligne; 
+             while((ligne=in.readLine())!=null) 
+              {
+                
+        
+
+                tabligne = ligne.split(";");
+                /*System.out.println(tabligne[1]);*/             
+                 
+                 
+                if ((revet.compareTo(tabligne[11])==0)) {
+                    double_prix=Double.parseDouble(tabligne[10]);
+                    prix_R=prix_R+double_prix;
+                    // System.out.println(double_prix);
+
+                 }
+               }
+
+              in.close();
+             }
+
+
+           catch(FileNotFoundException err){
+           //System.out.println( "Erreur :le fichier n’existe pas!\n "+err);
+           }
+
+           catch (IOException err){
+           //System.out.println(" Erreur :\n "+err);
+           }
+         
+       
+       
+       
+       return prix_R;
+   }
+   
+      public static double surf_revet(String revet)
+   {
+       double double_surface, surface; surface=0;
+       String [] tabligne = new String [30];
+       
+       
+      try 
+             {
+             BufferedReader in=new BufferedReader(new FileReader("memoire.txt"));
+             
+             String ligne; 
+             while((ligne=in.readLine())!=null) 
+              {
+                
+        
+
+                tabligne = ligne.split(";");
+                /*System.out.println(tabligne[1]);*/             
+                 
+                 
+                if ((revet.compareTo(tabligne[11])==0)) {
+                    double_surface=Double.parseDouble(tabligne[12]);
+                    surface=surface+double_surface;
+                    // System.out.println(double_prix);
+
+                 }
+               }
+
+              in.close();
+             }
+
+
+           catch(FileNotFoundException err){
+           //System.out.println( "Erreur :le fichier n’existe pas!\n "+err);
+           }
+
+           catch (IOException err){
+           //System.out.println(" Erreur :\n "+err);
+           }
+         
+       
+       
+       
+       return (surface);
+   }
+   
+   public static void main(String[] args) {
      
         //-------------------
         
@@ -606,6 +730,7 @@ public class Projet_56 {
            System.out.println(" 2 - ajouter piece ");
            System.out.println(" 3 - calcul du prix piece ");
            System.out.println(" 4 - ecrire prix piece ");
+           System.out.println(" 5 - ecrire prix revet ");
            System.out.println("");
            choisissateur=Lire.i();
            
@@ -688,6 +813,22 @@ public class Projet_56 {
                 }
                   
                
+                         
+           } 
+           
+           if (choisissateur==5)
+           {
+             
+              
+                System.out.println("Ecrire le prix du revetement : ");
+                revet=Lire.S();
+                
+                System.out.println("");
+
+               ecriture("surface de  " + revet , surf_revet(revet));
+               ecriture_espace(3);
+               ecriture("cout " + revet , prix_revet(revet));  
+               ecriture_espace(3);ecriture_espace(3);ecriture_espace(3);
                          
            } 
 
